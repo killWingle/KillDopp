1182633f7a32efd03f5b8447fc8e3b6cf5951fad
killWingle <hayaful11@gmail.com>
2024-06-10 21:51:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -245,7 +245,7 @@ function searchVideos(query, pageToken = '') {
 
 // 初回ロード時にランダム動画を表示
 window.addEventListener('load', function () {
-    searchVideos('HelpmeERINNNNNN');
+    searchVideos('Help me, ERINNNNNN!!');
 });
 
 // カスタム動画プレーヤーのコントロール
